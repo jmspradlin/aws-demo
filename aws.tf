1d20d0f8c2747863176a759277f23de1870257ed
Jeff Spradlin <Jeff Spradlin>
2023-04-25 15:04:15 -0500

    updating instance count

--- aws.tf
+++ aws.tf
@@ -17,7 +17,7 @@ module "instance" {
   source  = "app.terraform.io/jeff-spradlin-org/ec2-instance/aws"
   version = "1.1.3"
 
-  instance_count         = 5
+  instance_count         = 2
   instance_name          = "lab-test"
   subnet_id              = module.network.private_subnet_ids
   vpc_security_group_ids = [module.network.aws_security_group_id]
